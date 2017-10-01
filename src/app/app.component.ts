7cc5fb7e8614a9bf22e34c0f0f6bce10f1fb75ec
Taylor Kettle <tin.teapot@gmail.com>
2017-10-01 22:06:25 +1300

    more styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,12 @@ export class AppComponent implements OnInit {
 	public ngOnInit(): void {
 		// Create timeline
 		const items = new DataSet([
-			{ id: 1, content: 'item 1', title: 'title 1', start: '2013-04-20', group: 1, className: 'inpatientItem' },
-			{ id: 2, content: 'item 2', start: '2013-04-14', group: 3, className: 'midwifeItem' },
-			{ id: 3, content: 'item 3', start: '2013-04-18', group: 4, className: 'deliverItem'},
+			{ id: 1, content: 'item 1', title: 'title 1', start: '2013-04-20', end: '2013-04-21', group: 1, className: 'inpatientItem' },
+			{ id: 2, content: 'item 2', start: '2013-04-14', end: '2013-04-15', group: 3, className: 'midwifeItem' },
+			{ id: 3, content: 'item 3', start: '2013-04-18', end: '2013-04-19', group: 4, className: 'deliverItem'},
 			{ id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19', group: 2, className: 'outpatientItem' },
-			{ id: 5, content: 'item 5', start: '2013-04-25' },
-			{ id: 6, content: 'item 6', start: '2013-04-27' }
+			{ id: 5, content: 'item 5', start: '2013-04-25', end: '2013-04-26' },
+			{ id: 6, content: 'item 6', start: '2013-04-27', end: '2013-04-28' }
 		]);
 
 		const groups = new DataSet([
@@ -29,7 +29,8 @@ export class AppComponent implements OnInit {
 
 		const options = {
 			showMinorLabels: true,
-			zoomable: false
+			zoomable: false,
+			orientation: 'top',
 		};
 
 		const timelineContainer = document.getElementById('myTimeline');

01d0e6304a16b54c36d199371fa3fcd7262a2fe3
Taylor Kettle <tin.teapot@gmail.com>
2017-10-01 18:54:40 +1300

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,27 @@ export class AppComponent implements OnInit {
 	public ngOnInit(): void {
 		// Create timeline
 		const items = new DataSet([
-			{ id: 1, content: 'item 1', title: 'title 1', start: '2013-04-20' },
-			{ id: 2, content: 'item 2', start: '2013-04-14' },
-			{ id: 3, content: 'item 3', start: '2013-04-18' },
-			{ id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19' },
+			{ id: 1, content: 'item 1', title: 'title 1', start: '2013-04-20', group: 1, className: 'inpatientItem' },
+			{ id: 2, content: 'item 2', start: '2013-04-14', group: 3, className: 'midwifeItem' },
+			{ id: 3, content: 'item 3', start: '2013-04-18', group: 4, className: 'deliverItem'},
+			{ id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19', group: 2, className: 'outpatientItem' },
 			{ id: 5, content: 'item 5', start: '2013-04-25' },
 			{ id: 6, content: 'item 6', start: '2013-04-27' }
 		]);
+
+		const groups = new DataSet([
+			{ id: 1, content: 'Inpatient' },
+			{ id: 2, content: 'Outpatient' },
+			{ id: 3, content: 'Midwife' },
+			{ id: 4, content: 'Delivery' },
+		]);
+
+		const options = {
+			showMinorLabels: true,
+			zoomable: false
+		};
+
 		const timelineContainer = document.getElementById('myTimeline');
-		const timeline = new Timeline(timelineContainer, items, {});
+		const timeline = new Timeline(timelineContainer, items, groups, options);
 	}
 }
